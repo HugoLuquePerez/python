045c0a31e1db3711af7f829a52a54e117ecb78cb
HugoLuquePerez <hugoluque19102004@gmail.com>
2023-09-14 00:13:17 +0200

    first commit

---
+++ DSimplesVariables/DsimplesVariables.py
@@ -0,0 +1,29 @@
+""" Primer Hola Mundo """
+
+print("Hello World")
+
+""" Tipos de Datos en Python """
+
+# Entero (int)
+
+entero = 1024 # entero positivo y negativo
+neg_entero = -356789 # numero con signo menos (-) para indicar que es un valor negativo.
+
+# Flotante (float o double, float es mas pequeño aunque sea mayor precisión.)
+
+flotante =  3.14159265358979323846264
+decimal = .3e-2 # flotantes decimales separados por puntos (.). e representa la potencia del exponente
+exponencial = 1E+2 # exponencial se usa el E seguido de + o -.
+
+# Cadena de Caracteres (str)
+
+cadena = "Hola mundo" #' ' son comillas simples y "" son comillas dobles.
+caracter = 'A' # caracter solo puede ser una letra entre '' o "".
+
+# Booleano True or False
+
+booleano = True
+
+# Nulo None
+
+nulo=None
